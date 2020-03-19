24f34b93c2078365d7940ca0263474c6c8e03b9e
Henrique Ramos <henrique.ramos@avenuecode.com>
2020-03-19 17:28:27 -0300

    Create query and add apikey argument

---
+++ fetchData.py
@@ -0,0 +1,50 @@
+import requests
+import json
+import sys
+import errno
+import time
+
+headers = {'Authorization': 'Bearer {}'.format(sys.argv[1])}
+
+
+def runQuery(query):
+    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+    if request.status_code == 200:
+        return request.json()
+    else:
+      raise Exception('Error {}. {}'.format(request.status_code, query))
+
+
+query = '''
+{
+  user(login: "gvanrossum") {
+    repositories(first: 50) {
+      nodes {
+        name
+        primaryLanguage {
+          name
+        },
+        stargazers {
+          totalCount
+        },
+        watchers {
+          totalCount
+        },
+        createdAt,
+        forks {
+          totalCount
+        },
+        url
+      }
+    }
+  }
+}
+'''
+
+filename = 'output.json'
+nodes = runQuery(query)['data']['user']['repositories']['nodes']
+print(nodes)
+with open('output.json', 'w') as file:
+  json.dump([node for node in nodes if node['primaryLanguage'] is not None and node['primaryLanguage']['name'] == 'Python'], file)
+
+print('Written to file output.json')
