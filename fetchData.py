21694197293973606283c66416df74d1d500e251
Henrique Ramos <henrique.ramos@avenuecode.com>
2020-04-02 23:55:17 -0300

    Change fetchData

--- fetchData.py
+++ fetchData.py
@@ -1,54 +1,74 @@
 import requests
+import os
+import errno
 import json
 import sys
-import errno
-import time
+from string import Template
 
-headers = {'Authorization': 'Bearer {}'.format(sys.argv[1])}
+headers = {'Authorization': f'Bearer {sys.argv[1]}'}
 
-
-def runQuery(query):
+def runQuery(query, headers): 
     request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
     if request.status_code == 200:
         return request.json()
     else:
-      raise Exception('Error {}. {}'.format(request.status_code, query))
+        raise Exception(f'Query failed to run by returning code of {request.status_code}.')
 
-
-query = '''
+query = Template('''
 {
-  user(login: "gvanrossum") {
-    repositories(first: 50) {
-      nodes {
-        name
-        primaryLanguage {
-          name
-        },
-        stargazers(orderBy: {field: STARRED_AT, direction: DESC}) {
-          totalCount
-        },
-        watchers {
-          totalCount
-        },
-        releases {
-          totalCount
-        },
-        createdAt,
+search(query: "language:Python stars:>100", type: REPOSITORY, first: 99$AFTER) 
+  {
+  pageInfo {
+      hasNextPage
+      endCursor
+  }
+    nodes {
+      ... on Repository {
         forks {
           totalCount
-        },
-        url,
+        }
+        createdAt
+        watchers {
+          totalCount
+        }
+        url
+        stargazers {
+          totalCount
+        }
+        releases {
+          totalCount
+        }
+        nameWithOwner
+        name
         isFork
+        primaryLanguage {
+          name
+        }
       }
+    }
+    pageInfo {
+      endCursor
+      hasNextPage
     }
   }
 }
-'''
+''')
+
+result = runQuery(query.substitute(AFTER=''), headers)
+nodes = result['data']['search']['nodes']
+currentPage = 0
+hasNextPage  = result['data']['search']['pageInfo']['hasNextPage'] 
+
+
+while (currentPage < 10 and hasNextPage): 
+  cursor = result['data']['search']['pageInfo']['endCursor']
+  result = runQuery(query.substitute(AFTER = ', after: \"%s\"' % cursor), headers)
+  nodes += result['data']['search']['nodes']
+  hasNextPage  = result['data']['search']['pageInfo']['hasNextPage']
+  currentPage += 1
 
 filename = 'output.json'
-nodes = runQuery(query)['data']['user']['repositories']['nodes']
-print(nodes)
-with open('output.json', 'w') as file:
-  json.dump([node for node in nodes if node['primaryLanguage'] is not None and node['primaryLanguage']['name'] == 'Python' and node['isFork'] != True], file)
+with open(filename, 'w') as file:
+  json.dump(nodes, file)
 
-print('Written to file output.json')
+print(f'Written to file {filename}')
--- linesOfCode.py
+++ linesOfCode.py
@@ -6,15 +6,17 @@ with open('output.json', 'r') as file:
   data = json.load(file)
 
 def getLinesOfCode(repo):
-  os.system(f'git clone {repo["url"]} {repo["name"]}')
+  print(f"Cloning {repo['name']}")
+  os.system(f'git clone --quiet {repo["url"]} {repo["name"]}')
   loc = os.popen(f'cd {repo["name"]} && git ls-files | grep .py | xargs cat | wc -l').read()
   os.system(f'rm -rf {repo["name"]}')
-  
+  print(f"{repo['name']} has {loc[0: -1]} lines of code.")
+
   if len(loc) > 0:
     return int(loc[0:-1])
   else:
     return int(0)
 
 
-with open('output.json', 'w') as file:
+with open('linesOfCode.json', 'w') as file:
   json.dump([dict(repo, **{"linesOfCode": getLinesOfCode(repo)}) for repo in data], file)
